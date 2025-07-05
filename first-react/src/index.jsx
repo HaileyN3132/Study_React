6bcf9b103b1ca676dd38efabafb0ded611ad94ef
Hailey <ngohailey02@gmail.com>
2025-07-05 17:06:13 -0400

    Create first react app

---
+++ first-react/src/index.jsx
@@ -0,0 +1,25 @@
+import { createRoot } from "react-dom/client";
+const root = createRoot(document.getElementById("root"));
+
+function Page() {
+  return (
+    <div>
+      <header>
+        <img src="src/assets/react.svg" alt="React logo" />
+      </header>
+
+      <main>
+        <h1>Reasons I'm excited to learn React</h1>
+
+        <ol>
+          <li>Get familiar with front end technologies</li>
+          <li>Curiuous about this framework</li>
+          <li>Be able to add React in my personal project</li>
+        </ol>
+      </main>
+      <footer>© 2025 Hailey development. All rights reserved.</footer>
+    </div>
+  );
+}
+
+root.render(<Page />);
