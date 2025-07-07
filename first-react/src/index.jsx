7716e3dc74a7f322a86af2ed81a5e5d5ea29120b
Hailey <ngohailey02@gmail.com>
2025-07-07 18:42:49 -0400

    (Challenge 3) - Styling with Classes

--- first-react/src/index.jsx
+++ first-react/src/index.jsx
@@ -4,17 +4,14 @@ const root = createRoot(document.getElementById("root"));
 /**
  * Challenge:
  *
- * Using flexbox, line up the `li`s horizontally and put them inline
- * with the React logo.
- *
- * NOTE: for practice's sake, don't select any
- * elements, but use classes for all styling.
+ * Move the `width` style off the JSX and into CSS with a dedicated
+ * className on the img element, and change the width to 55px instead
  */
 
 function Header() {
   return (
     <header className="header">
-      <img src="src/assets/react.svg" alt="React logo" />
+      <img className="nav-logo" src="src/assets/react.svg" alt="React logo" />
       <nav>
         <ul className="nav-list">
           <li className="nav-list-item">Pricing</li>
