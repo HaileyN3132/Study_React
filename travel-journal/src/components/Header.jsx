f8072a9b5b693e4624f220bea2fde880bb950a35
Hailey <ngohailey02@gmail.com>
2025-07-12 14:00:35 -0400

    Challenge - Header created and styling

---
+++ travel-journal/src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header className="header">
+      <img src="src/assets/Globe.png" alt="Globe logo" />
+      <span>my travel journal</span>
+    </header>
+  );
+}
