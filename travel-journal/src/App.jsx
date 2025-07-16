390ad4d2b3a05a817cd49b30ccdfacc748091f02
Hailey <ngohailey02@gmail.com>
2025-07-16 18:37:34 -0400

    Hardcode information for the component

--- travel-journal/src/App.jsx
+++ travel-journal/src/App.jsx
@@ -1,8 +1,10 @@
+import Entry from "./components/Entry";
 import Header from "./components/Header";
 export default function App() {
   return (
     <>
       <Header />
+      <Entry />
     </>
   );
 }
--- travel-journal/src/components/Header.jsx
+++ travel-journal/src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className="header">
-      <img src="src/assets/Globe.png" alt="Globe logo" />
+      <img src="src/assets/globe.png" alt="Globe logo" />
       <span>my travel journal</span>
     </header>
   );
