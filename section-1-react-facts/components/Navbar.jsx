394ce14755db5b9ed39a0ebf0d9c3d6ae0871bb4
Hailey <ngohailey02@gmail.com>
2025-07-23 14:01:26 -0400

    Rename Section 1

---
+++ section-1-react-facts/components/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+  return (
+    <header className="header">
+      <nav className="nav-bar">
+        <img className="react-logo" src="src/assets/react.svg" alt="" />
+        <span>ReactFacts</span>
+      </nav>
+    </header>
+  );
+}
