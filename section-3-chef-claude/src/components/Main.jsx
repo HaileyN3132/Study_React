56b420d0d2f51cba1accd315669d6ddf87f56c48
Hailey <ngohailey02@gmail.com>
2025-07-26 12:50:05 -0400

    Added handle form submit

--- section-3-chef-claude/src/components/Main.jsx
+++ section-3-chef-claude/src/components/Main.jsx
@@ -1,14 +1,31 @@
 export default function Main() {
+  const ingredients = ["Chicken", "Oregano", "Tomatoes"];
+  const ingredientsListItems = ingredients.map((ingredient) => (
+    <li key={ingredient}>{ingredient}</li>
+  ));
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const newIngredient = formData.get("ingredient");
+
+    ingredients.push(newIngredient);
+    console.log(ingredients);
+  }
+
   return (
     <main className="search-container">
-      <form className="add-ingredient-form" action="">
+      <form onSubmit={handleSubmit} className="add-ingredient-form" action="">
         <input
+          name="ingredient"
           type="text"
           placeholder="e.g. oregano"
           aria-label="Add ingredient"
         />
         <button>Add ingredients</button>
       </form>
+
+      <ul>{ingredientsListItems}</ul>
     </main>
   );
 }
