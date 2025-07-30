266f4500b79a28a43384e4cb40cbc1703a21f9df
Hailey <ngohailey02@gmail.com>
2025-07-30 11:36:32 -0400

    Added Input Type Radio To The Form

--- react-playground/src/FormBasic.jsx
+++ react-playground/src/FormBasic.jsx
@@ -2,8 +2,10 @@ export default function FormBasic() {
   function signup(formData) {
     const email = formData.get("email");
     const password = formData.get("password");
+    const radio = formData.get("employmentStatus");
     console.log(email);
     console.log(password);
+    console.log(radio);
   }
 
   return (
@@ -11,10 +13,44 @@ export default function FormBasic() {
       <h1>Signup form</h1>
       <form action={signup}>
         <label htmlFor="email">Email</label>
-        <input id="email" type="email" name="email" placeholder="Email" />
+        <input
+          id="email"
+          type="email"
+          name="email"
+          placeholder="Email"
+          defaultValue={"h@gmail.com"}
+        />
         <br />
         <label htmlFor="password">Password</label>
-        <input id="password" type="password" name="password" />
+        <input
+          id="password"
+          type="password"
+          name="password"
+          defaultValue={123456}
+        />
+        <br />
+        <label htmlFor="discription"></label> Discription
+        <textarea
+          name="discription"
+          id="discription"
+          defaultValue={"This is description"}
+        ></textarea>
+        <br />
+        <fieldset>
+          <legend>Employment Status:</legend>
+          <label>
+            <input type="radio" name="employmentStatus" value="unemployed" />
+            Unemployed
+          </label>
+          <label>
+            <input type="radio" name="employmentStatus" value="part-time" />
+            Part-time
+          </label>
+          <label>
+            <input type="radio" name="employmentStatus" value="full-time" />
+            Full-time
+          </label>
+        </fieldset>
         <br />
         <button>Submit</button>
       </form>
