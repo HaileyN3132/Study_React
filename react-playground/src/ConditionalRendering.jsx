85a217a0d10e4bed5f081050891185ab0c7d7f6f
Hailey <ngohailey02@gmail.com>
2025-07-30 12:55:20 -0400

    Practice conditional rendering

---
+++ react-playground/src/ConditionalRendering.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function ConditionalRendering() {
+  const [unreadMessages, setUnreadMessages] = React.useState(["a", "b"]);
+
+  return (
+    <div>
+      {unreadMessages.length > 0 && (
+        <h1>You have {unreadMessages.length} unread messages! </h1>
+      )}
+
+      {unreadMessages.length == 0 && <h1>You have no unread messages</h1>}
+    </div>
+  );
+}
--- react-playground/src/main.jsx
+++ react-playground/src/main.jsx
@@ -4,12 +4,13 @@ import "./index.css";
 import ComplexObject from "./ComplexObject.jsx";
 import FormBasic from "./FormBasic";
 import JokeApp from "./JokeApp.jsx";
+import ConditionalRendering from "./ConditionalRendering.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/*<ComplexObject />*/}
     {/*<FormBasic />*/}
-
-    <JokeApp />
+    {/*<JokeApp />*/}
+    <ConditionalRendering />
   </StrictMode>
 );
