f9ad0e994e7244fa106f8a504add153ea199696d
Hailey <ngohailey02@gmail.com>
2025-08-09 18:47:53 -0400

    Intro to Share State

--- react-playground/src/SoundPads.jsx
+++ react-playground/src/SoundPads.jsx
@@ -2,10 +2,22 @@ import React from "react";
 import padsData from "./data/pads";
 import Pad from "./components/Pad";
 
+/**
+ * Challenge: Create a toggle() function that logs
+ * "clicked!" to the console
+ *
+ * Pass that function down to each of the Pad components
+ * and set it up so when they get clicked, the function runs
+ */
+
 export default function SoundPads(props) {
+  function toggle() {
+    console.log("Clicked");
+  }
+
   const [pads, setPads] = React.useState(padsData);
   const buttonElemens = pads.map((pad) => (
-    <Pad key={pad.id} color={pad.color} on={pad.on} />
+    <Pad key={pad.id} color={pad.color} on={pad.on} toggle={toggle} />
   ));
 
   return (
--- react-playground/src/components/Pad.jsx
+++ react-playground/src/components/Pad.jsx
@@ -1,25 +1,11 @@
 import React from "react";
 
 export default function Pad(props) {
-  /**
-   * Challenge: Create state controlling whether
-   * this pad is "on" or "off". Use the incoming
-   * `props.on` to determine the initial state.
-   *
-   * Create an event listener so when the pad is clicked,
-   * it toggles from "on" to "off".
-   *
-   * Goal: clicking each pad should toggle it on and off.
-   */
-
   const [on, setOn] = React.useState(props.on);
-  function toggle() {
-    setOn((prevOn) => !prevOn);
-  }
 
   return (
     <button
-      onClick={toggle}
+      onClick={props.toggle}
       className={`pad-btn ${on ? "on" : undefined}`}
       style={{ backgroundColor: props.color }}
     ></button>
