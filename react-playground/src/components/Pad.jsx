484902d2dd934ab38a6cde52408cfb34d1777580
Hailey <ngohailey02@gmail.com>
2025-08-09 18:19:42 -0400

    Handle props and state by using local state

--- react-playground/src/components/Pad.jsx
+++ react-playground/src/components/Pad.jsx
@@ -1,13 +1,26 @@
+import React from "react";
+
 export default function Pad(props) {
   /**
-   * Challenge part 3:
-   * Our buttons got turned off by default! Update the code
-   * so if the button is "on", it has the className of "on".
+   * Challenge: Create state controlling whether
+   * this pad is "on" or "off". Use the incoming
+   * `props.on` to determine the initial state.
+   *
+   * Create an event listener so when the pad is clicked,
+   * it toggles from "on" to "off".
+   *
+   * Goal: clicking each pad should toggle it on and off.
    */
+
+  const [on, setOn] = React.useState(props.on);
+  function toggle() {
+    setOn((prevOn) => !prevOn);
+  }
 
   return (
     <button
-      className={`pad-btn ${props.on ? "on" : null}`}
+      onClick={toggle}
+      className={`pad-btn ${on ? "on" : undefined}`}
       style={{ backgroundColor: props.color }}
     ></button>
   );
